cb0c8617d43e14797dfee9f1d02955aefb21591a
Pandalister <thom.kyu@gmail.com>
2016-12-08 22:11:05 -0400

    Changement accueil 01

---
+++ function/slider.php
@@ -0,0 +1,44 @@
+                <div>
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      <!-- Indicators -->
+                      <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                      </ol>
+
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                          <img src="/taste/images/back1.jpg" alt="1">
+                          <div class="carousel-caption">
+                            <img class="logo" src="./images/taste.png" id="tasteLogo" alt="Taste"/>
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <img src="/taste/images/back2.jpg" alt="2">
+                          <div class="carousel-caption">
+                            <img class="logo" src="./images/taste.png" id="tasteLogo" alt="Taste"/>
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <img src="/taste/images/back4.jpg" alt="3">
+                          <div class="carousel-caption">
+                            <img class="logo" src="./images/taste.png" id="tasteLogo" alt="Taste"/>
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+                </div>
--- index.php
+++ index.php
@@ -17,55 +17,29 @@
         <!---------------------------------------------------------------->
         <div class="loader"></div>
         <?php include('./function/header.php'); ?>
-            <div id="container">
-                <div>
-                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                      <!-- Indicators -->
-                      <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                      </ol>
-
-                      <!-- Wrapper for slides -->
-                      <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                          <img src="/taste/images/back1.jpg" alt="1">
-                          <div class="carousel-caption">
-                            <img class="logo" src="./images/taste.png" id="tasteLogo" alt="Taste"/>
-                          </div>
-                        </div>
-
-                        <div class="item">
-                          <img src="/taste/images/back2.jpg" alt="2">
-                          <div class="carousel-caption">
-                            <img class="logo" src="./images/taste.png" id="tasteLogo" alt="Taste"/>
-                          </div>
-                        </div>
-
-                        <div class="item">
-                          <img src="/taste/images/back4.jpg" alt="3">
-                          <div class="carousel-caption">
-                            <img class="logo" src="./images/taste.png" id="tasteLogo" alt="Taste"/>
-                          </div>
-                        </div>
-                      </div>
-
-                      <!-- Controls -->
-                      <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                      <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </a>
+        <div id="container">
+            <?php include('./function/slider.php'); ?>
+            <!---------------------------------------------------------------->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-1">
+                        TEst
                     </div>
                 </div>
-                <!---------------------------------------------------------------->
-                <div>
-                    <p>
-                    </p>
+            </div>
+            <div id="content1" class="container">
+                <div class="row">
+                    <div class="col-sm-4 col-lg-6">
+                            <h2>
+                                Une recette par passion
+                            </h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                            </p>
+                    </div>
+                    <div id="imgAC1" class="col-sm-1 col-lg-6">
+                        <img id="ac1" src="./images/01.jpg"/>
+                    </div>
                 </div>
             </div>
         </div>
